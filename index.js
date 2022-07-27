48e17b0569cf0336e6a6672d2035eb859caeeb7f
Ziyad Seykhanov <zziyadkhanov@gmail.com>
2022-07-27 19:12:29 +0400

    checking

---
+++ index.js
@@ -0,0 +1,30 @@
+const updateParserResult = (state, result) => ({
+  ...state, result 
+ });
+const updateParserState = (state, index, result) => ({ 
+ ...state, index, result 
+});
+const updateParserError = (state, errorMsg) => ({ 
+ ...state, isError: true, error: errorMsg 
+});
+
+class Parser {
+  constructor(parserStateTransformerFn) {
+    this.parserStateTransformerFn = parserStateTransformerFn;
+  }
+
+  run(targetString) {
+    const initialState = {
+      targetString,
+      index: 0,
+      result: null,
+      isError: false,
+      error: null,
+    };
+
+    return this.parserStateTransformerFn(initialState);
+  }
+  
+}
+
+
