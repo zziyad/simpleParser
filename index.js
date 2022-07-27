1b0e82728c62e53ea4f35d87353e4be2124d97b2
Ziyad Seykhanov <zziyadkhanov@gmail.com>
2022-07-27 19:03:03 +0400

    create Parser class

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ const updateParserState = (state, index, result) => ({
 const updateParserError = (state, errorMsg) => ({ 
  ...state, isError: true, error: errorMsg 
 });
+
+class Parser {
+  constructor(parserStateTransformerFn) {
+    this.parserStateTransformerFn = parserStateTransformerFn;
+  }
+
+  run(targetString) {
+    const initialState = {
+      targetString,
+      index: 0,
+      result: null,
+      isError: false,
+      error: null,
+    };
+
+    return this.parserStateTransformerFn(initialState);
+  }
+}
